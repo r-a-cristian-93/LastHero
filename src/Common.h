03a45c289ae785939a698c543fefa144cf615013
unknown <C:\Users\Administrator\AppData\Roaming\The Bat!>
2022-01-30 15:51:58 +0200

    Removed unused structs
    Former-commit-id: 66f5aedff375812e172bac6a3631641eb1b5c5ba

--- src/Common.h
+++ src/Common.h
@@ -17,44 +17,4 @@ struct AppConfig {
 	float cam_speed, cam_treshold;
 };
 
-
-struct PlayerConfig {
-	int velocity;
-	float shape_radius;
-	int collision_radius;
-	int fill_r, fill_g, fill_b;
-	int out_r, out_g, out_b, out_thk;
-	size_t vertices;
-};
-
-struct EnemyConfig {
-	int velocity_min, velocity_max;
-	float shape_radius;
-	int collision_radius;
-	int out_r, out_g, out_b, out_thk;
-	int child_lifespan;
-	size_t vertices_min, vertices_max;
-	int spawn_interval;
-};
-
-struct BulletConfig {
-	int velocity;
-	float shape_radius;
-	int collision_radius;
-	int fill_r, fill_g, fill_b;
-	int out_r, out_g, out_b, out_thk;
-	int lifespan;
-	size_t vertices;
-};
-
-struct MissleConfig {
-	int velocity;
-	float shape_radius;
-	int collision_radius;
-	int fill_r, fill_g, fill_b;
-	size_t vertices;
-	int cooldown;
-	int lifespan;
-};
-
 #endif
