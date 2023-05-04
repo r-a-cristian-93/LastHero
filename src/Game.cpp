d9ac1776678828277fa681e7e2194f1572335a62
Alex <r.a.cristian.93@gmail.com>
2023-05-04 12:17:14 +0300

    merged App into Game in order to remove unneeded level of abstraction

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,10 +6,126 @@
 #include "SceneGameOver.h"
 #include "SceneScore.h"
 #include "SceneSettings.h"
+#include "Profiler.h"
 
 Game::Game()
+	:running(true)
+	,current_scene(nullptr)
+	,render_states(sf::RenderStates::Default)
+	,next_scene(0)
 {
 	init();
+}
+
+void Game::run() {
+	while(running) {
+		PROFILE_SCOPE("MAIN_GAME_LOOP");
+
+		if (window->isOpen()) {
+			sUserInput();
+
+			screen_tex->clear();
+
+			if (current_scene) {
+				current_scene->update();
+				sSceneFade();
+			}
+
+			screen_tex->display();
+
+			window->draw(*screen_sprite, render_states);
+			window->display();
+
+			sChangeScene();
+		}
+	}
+}
+
+void Game::sUserInput() {
+	PROFILE_FUNCTION();
+
+	sf::Event event;
+	Action::Code action_code = Action::NONE;
+
+	while (window->pollEvent(event)) {
+		if (event.type == sf::Event::Closed) {
+			running = false;
+		}
+		else if (event.type == sf::Event::KeyPressed) {
+			action_code = act_mgr->getCode(ActionManager::DEV_KEYBOARD, event.key.code);
+
+			if (action_code) {
+				Action action(action_code, Action::TYPE_START);
+				current_scene->doAction(action);
+			}
+		}
+		else if (event.type == sf::Event::KeyReleased) {
+			action_code = act_mgr->getCode(ActionManager::DEV_KEYBOARD, event.key.code);
+
+			if (action_code) {
+				Action action(action_code, Action::TYPE_END);
+				current_scene->doAction(action);
+			}
+		}
+	}
+}
+
+void Game::sChangeScene() {
+	if (next_scene) {
+		setScene(next_scene);
+		next_scene = 0;
+	}
+}
+
+void Game::sSceneFade() {
+	unsigned char * current_fade_frames = current_scene->current_fade_frames;
+	FadeType& fade = current_scene->fade;
+	const unsigned int* fade_frames = current_scene->fade_frames;
+	bool& music_fade_out = current_scene->music_fade_out;
+
+	switch (fade) {
+		case FADE::IN: {
+			current_fade_frames[fade]++;
+			unsigned char c = current_fade_frames[fade] * (255/fade_frames[fade]);
+			screen_sprite->setColor({c, c, c});
+
+			if (current_fade_frames[fade] >= fade_frames[fade]) {
+				screen_sprite->setColor({255, 255, 255});
+				fade = FADE::NONE;
+			}
+
+		}
+		break;
+		case FADE::OUT: {
+			if (current_fade_frames[fade] > 0) current_fade_frames[fade]--;
+			unsigned char c = current_fade_frames[fade] * (255/fade_frames[fade]);
+
+			if (music_fade_out && current_scene->next_scene != GAME_SCENE::SETTINGS) {
+				float v = 0;
+
+				if (current_fade_frames[fade] > 0) {
+					v = current_fade_frames[fade] * (app_conf->music_volume/fade_frames[fade]);
+				}
+				snd_mgr->setBgMusicVolume(v);
+			}
+
+			screen_sprite->setColor({c, c, c});
+
+			if (current_fade_frames[fade] == 0) {
+				fade = FADE::NONE;
+				screen_sprite->setColor({0, 0, 0});
+				setNextScene(current_scene->next_scene);
+			}
+
+
+		}
+		break;
+	}
+}
+
+
+void Game::setNextScene(size_t id) {
+	next_scene = id;
 }
 
 void Game::init() {
@@ -72,4 +188,7 @@ void Game::setScene(size_t id) {
 	}
 }
 
-Game::~Game() {}
+Game::~Game() {
+	delete current_scene;
+}
+
--- src/Game.h
+++ src/Game.h
@@ -1,15 +1,26 @@
 #ifndef GAME
 #define GAME
 
-#include "App.h"
+#include "Scene.h"
 
-class Game: public App {
-private:
+class Game {
+protected:
+	Scene* current_scene;
+	size_t next_scene;
+	sf::RenderStates render_states;
 
-	void init() override;
-	void setScene(size_t id) override;
+	void init();
+	void setScene(size_t id);
+
+	void sSceneFade();
+	void sUserInput();
+	void sChangeScene();
 
 public:
+	bool running;
+
+	void run();
+	void setNextScene(size_t id);
 
 	Game();
 	~Game();
